c4aadd29480af5e4eb4a1aedb388c3230d3b3b83
gowthambhargav <gowthambhargav26@gmail.com>
2024-07-23 12:23:47 +0530

    buildType apk

--- utils/fetchDataFromUrl.js
+++
@@ -1,9 +0,0 @@
-export const fetchDataFromUrl = async (url) => {
-  try {
-    const response = await fetch(url);
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error(error);
-  }
-}
